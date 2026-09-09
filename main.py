6646aea668a61849e7ecd8b27e67564a72212da9
KamenskiyIlya <kamenskj.ilya@gmail.com>
2026-09-09 23:16:35 +0300

    Setup bullet stop when encountering an obstacle

--- main.py
+++ main.py
@@ -64,6 +64,12 @@ async def fire(
     curses.beep()
 
     while 0 < row < max_row and 0 < column < max_column:
+        for obstacle in OBSTACLES:
+            if obstacle.has_collision(
+                row,
+                column,
+            ):
+                return
         canvas.addstr(round(row), round(column), symbol)
         await asyncio.sleep(0)
         canvas.addstr(round(row), round(column), ' ')
